bfeac284a8080e7cbe7db8f38888010ee33cbb9e
=tuyenhm <tuyenhm93@gmail.com>
2016-08-11 02:27:10 +0300

    Changed user's password

--- ism.constants.php
+++ ism.constants.php
@@ -1,7 +1,7 @@
 <?php
 define ("DB_SERVER", "localhost");
 define ( 'DB_USER', 'root' );
-define ( 'DB_PASSWORD', '' );
+define ( 'DB_PASSWORD', '215241290' );
 define ( 'DB_NAME', 'awn' );
 
 define("TBL_USERS", "awn.users");
